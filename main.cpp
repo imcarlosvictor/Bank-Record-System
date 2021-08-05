45f2db7fa4098d836c61af15d2daa2b115f88994
Carlos Victor Torres <carlosvictortorres96@gmail.com>
2021-08-05 16:24:01 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+
+struct Account
+{
+
+};
+
+void add_record();
+void display_data();
+void search_record();
+void edit_record();
+void delete_record();
+
+int main()
+{
+  return 0;
+}
+
+
+void add_record()
+{
+
+}
+
+void display_data()
+{
+
+}
+
+void search_record()
+{
+
+}
+
+void edit_record()
+{
+
+}
+
+void delete_record()
+{
+
+}
