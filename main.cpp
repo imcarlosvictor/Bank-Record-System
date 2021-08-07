d270dffc5daedcdc195f8caf87859f535ad23da1
Carlos Victor Torres <carlosvictortorres96@gmail.com>
2021-08-07 19:34:15 -0400

    checkpoint: edit_record() completed

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ struct Account add_record(std::string,  double, double);
 void display_data(std::vector<struct Account>);
 void search_record(std::vector<struct Account>, int);
 void create_account(std::vector<struct Account>&);
-void edit_record();
+void edit_record(std::vector<struct Account>&, int);
 void delete_record();
 
 int main()
@@ -43,16 +43,19 @@ int main()
         display_data(account_users);
         continue;
       case 2:
-        std::cout << "Enter user account number: ";
-        int acc_id;
-        std::cin >> acc_id;
-        search_record(account_users, acc_id);
+        std::cout << "Enter user ID: ";
+        int search_acc_id;
+        std::cin >> search_acc_id;
+        search_record(account_users, search_acc_id);
         continue;
       case 3:
         create_account(account_users);
         continue;
       case 4:
-        edit_record();
+        std::cout << "Enter user ID: ";
+        int edit_acc_id;
+        std::cin >> edit_acc_id;
+        edit_record(account_users, edit_acc_id);
         continue;
       case 5:
         delete_record();
@@ -68,14 +71,15 @@ int main()
   return 0;
 }
 
-void display_data(std::vector<struct Account> acc_users)
+void display_data(std::vector<struct Account> accnt_users)
 {
   int count = 0;
   std::cout<< "-----------------------------------------------------------------------------------------\n";
   std::cout << "|\tID\t|\tName\t\t|\tChequings\t|\tSavings\t\t|\n";
   std::cout<< "-----------------------------------------------------------------------------------------\n";
-  for (int i=0; i<acc_users.size(); ++i) {
-    std::cout << "|     " << acc_users[i].account_id << "\t|\t" << acc_users[i].name << "\t\t|\t " << acc_users[i].chequings_account << "\t\t|\t" << acc_users[i].savings_account << "\t\t|\n";
+  for (int i=0; i<accnt_users.size(); ++i) {
+    std::cout << "|     " << accnt_users[i].account_id << "\t|\t" << accnt_users[i].name << "\t\t|\t " << accnt_users[i].chequings_account << "\t\t|\t" << accnt_users[i].savings_account << "\t\t|\n";
+    std::cout<< "-----------------------------------------------------------------------------------------\n";
     ++count;
   }
 
@@ -83,18 +87,18 @@ void display_data(std::vector<struct Account> acc_users)
     std::cout << "No existing users";
 }
 
-void search_record(std::vector<struct Account> acc_users, int acc_id)
+void search_record(std::vector<struct Account> accnt_users, int accnt_id)
 {
   /* std::cout << id.name << std::endl; */
   bool user_found = false;
-  for (int i=0; i<acc_users.size(); ++i) {
-    auto existing_user = acc_users[i].account_id == acc_id;
+  for (int i=0; i<accnt_users.size(); ++i) {
+    auto existing_user = accnt_users[i].account_id == accnt_id;
     if (existing_user) {
       user_found = true;
-      std::cout << "Name: " << acc_users[i].name << std::endl;
-      std::cout << "User ID: " << acc_users[i].account_id << std::endl;
-      std::cout << "Chequings: " << '$' << acc_users[i].chequings_account << std::endl;
-      std::cout << "Savings: " << '$' << acc_users[i].savings_account << std::endl;
+      std::cout << "Name: " << accnt_users[i].name << std::endl;
+      std::cout << "User ID: " << accnt_users[i].account_id << std::endl;
+      std::cout << "Chequings: " << '$' << accnt_users[i].chequings_account << std::endl;
+      std::cout << "Savings: " << '$' << accnt_users[i].savings_account << std::endl;
     }
   }
   if (!user_found)
@@ -108,7 +112,7 @@ void create_account(std::vector<struct Account> &vect)
       char new_account;
       std::string name;
       double chequings = 0;
-      double savings = 0;;
+      double savings = 0;
 
       std::cout << "\nPlease enter your name: ";
       std::cin >> name;
@@ -124,13 +128,13 @@ void create_account(std::vector<struct Account> &vect)
       int account_id =  rand() % ((199999 - 100000) + 1) + 100000;
 
       // Create and return account
-      struct Account new_acc = {name, account_id, chequings, savings};
+      struct Account new_accnt = {name, account_id, chequings, savings};
       std::cout << "\nAccount created successfully " << std::endl;
 
       // Store account
-      vect.push_back(new_acc);
+      vect.push_back(new_accnt);
 
-      search_record(vect, new_acc.account_id);
+      search_record(vect, new_accnt.account_id);
 
       // Ask if user wants to create another account
       std::cout << "\nAdd another account? (y/n): ";
@@ -148,9 +152,51 @@ void create_account(std::vector<struct Account> &vect)
   }
 }
 
-void edit_record()
+void edit_record(std::vector<struct Account> &accnt_users, int accnt_id)
 {
+  // Print User info
+  for (int i=0; i<accnt_users.size(); ++i) {
+    bool existing_user = accnt_users[i].account_id == accnt_id;
+    if (existing_user) {
+      std::cout << "Name: " << accnt_users[i].name << std::endl;
+      std::cout << "User ID: " << accnt_users[i].account_id << std::endl;
+      std::cout << "Chequings: " << '$' << accnt_users[i].chequings_account << std::endl;
+      std::cout << "Savings: " << '$' << accnt_users[i].savings_account << std::endl;
+    }
+  } 
 
+  std::string new_name;
+  double new_cheque_balance, new_savings_balance;
+
+  std::cout << "\nEnter name: ";
+  std::cin >> new_name;
+  std::cout << "Enter chequing balance: ";
+  std::cin >> new_cheque_balance;
+  std::cout << "Enter savings balance: ";
+  std::cin >> new_savings_balance;
+
+  // Display account with new info
+  std::cout << "Name: " << new_name << std::endl;
+  std::cout << "Chequings: $" << new_cheque_balance<< std::endl;
+  std::cout << "Savings: $" << new_savings_balance << std::endl;
+
+  // Confirm user change
+  char user_change;
+  std::cout << "Confirm change? (y/n): ";
+  std::cin >> user_change;
+  if (user_change == 'y') {
+    for (int i=0; i<accnt_users.size(); ++i) {
+      bool existing_user = accnt_users[i].account_id == accnt_id;
+      if (existing_user) {
+        accnt_users[i].name = new_name;
+        accnt_users[i].chequings_account = new_cheque_balance;
+        accnt_users[i].savings_account = new_savings_balance;
+        std::cout << "\nSuccessfully Updated profile\n";
+      }
+    }
+  } else {
+    std::cout << "No changes made" << std::endl;
+  }
 }
 
 void delete_record()
